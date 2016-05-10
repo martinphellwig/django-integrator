6c4eda4a0d1b5e30273cabe4b56e2a15670090ae
Martin P. Hellwig" <martin.hellwig@gmail.com>
2016-05-10 16:12:16 +0100

    Some changes to prepare for future release 1.1.2 (2)

--- setup.py
+++ setup.py
@@ -3,14 +3,13 @@ PyPi Setup file.
 """
 # pylint: disable=no-name-in-module, import-error
 from distutils.core import setup
-
 VERSION = '0.1.2'
 BASE_URL = 'https://bitbucket.org/hellwig/django-integrator'
 
 setup(
   name = 'Django-Integrator',
   packages = ['django_integrator'],
-  version = '0.1.1',
+  version = VERSION,
   description = 'Tool for integrating compliant 3rd party django apps',
   author = 'Martin P. Hellwig',
   author_email = 'martin.hellwig@gmail.com',
