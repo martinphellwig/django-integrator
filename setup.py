80f75aed5074ee89716209ef28a7d7c4ff325a10
Martin P. Hellwig <martin.hellwig@gmail.com>
2016-05-26 00:00:40 +0100

    add functionality and command line script to create django integrator compliant project.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ PyPi Setup file.
 """
 # pylint: disable=no-name-in-module, import-error
 from distutils.core import setup
-VERSION = '0.1.2'
+VERSION = '0.1.3'
 BASE_URL = 'https://bitbucket.org/hellwig/django-integrator'
 
 setup(
@@ -19,5 +19,9 @@ setup(
   license = 'BSD',
   classifiers = ['Programming Language :: Python :: 3',],
   install_requires = ['django'],
+   entry_points = {
+        'console_scripts':\
+   ['mk_django_integrator_app=django_integrator.mk_django_integrator_app:main'],
+    }
 )
 
