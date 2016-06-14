995e578880e38bc34799ff63e53a032d54f868f6
Martin P. Hellwig <martin.hellwig@gmail.com>
2016-06-14 23:27:36 +0100

    use shield.io badge for codeship  as it is svg

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ PyPi Setup file.
 """
 # pylint: disable=no-name-in-module, import-error
 from setuptools import setup
-VERSION = '1.0.0.1'
+VERSION = '1.0.0.2'
 BASE_URL = 'https://bitbucket.org/hellwig/django-integrator'
 SCRIPT = 'django-integrator-create='\
          'django_integrator_script.make_application:main'
